f608492af70f60c5c8674a270a1bd9dc27e34d8d
dawnIceZhu <Hello16369@163.com>
2022-07-06 22:26:48 +0800

    feat: switch

--- branch.go
+++ branch.go
@@ -15,6 +15,33 @@ func judge() {
 	}
 }
 
+// switch
+func eval(score int) string {
+	g := ""
+	switch {
+	case score < 0 || score > 100:
+		panic(fmt.Sprintf("Wrong scope: %d", score))
+	case score < 60:
+		g = "F"
+	case score < 80:
+		g = "C"
+	case score < 90:
+		g = "B"
+	case score <= 100:
+		g = "A"
+	}
+	return g
+}
+
 func main() {
 	judge()
+	fmt.Println(
+		eval(0),
+		eval(59),
+		eval(60),
+		eval(82),
+		eval(99),
+		eval(100),
+		eval(101),
+	)
 }
