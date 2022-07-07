0c8b168fb8b662dcab768ad2026b3bfda32f0d17
dawnIceZhu <Hello16369@163.com>
2022-07-07 20:59:32 +0800

    feat: 函数式编程

--- func.go
+++ func.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"runtime"
+)
 
 // 函数基础使用
 func eval(a, b int, op string) (int, error) {
@@ -25,6 +30,18 @@ func div(a, b int) (q, r int) {
 	return a / b, a % b
 }
 
+// 函数式编程写法
+func apply(op func(int, int) int, a, b int) int {
+	p := reflect.ValueOf(op).Pointer()
+	opName := runtime.FuncForPC(p).Name()
+	fmt.Printf("Calling function %s with args (%d, %d)\n", opName, a, b)
+	return op(a, b)
+}
+
+//func pow(a, b int) int {
+//	return int(math.Pow(float64(a), float64(b)))
+//}
+
 func main() {
 	if result, err := eval(13, 3, "X"); err != nil {
 		fmt.Println("Error: ", err)
@@ -33,4 +50,8 @@ func main() {
 	}
 	q, r := div(13, 3)
 	fmt.Println(q, r)
+	fmt.Println(apply(
+		func(a int, b int) int {
+			return int(math.Pow(float64(a), float64(b)))
+		}, 3, 4))
 }
