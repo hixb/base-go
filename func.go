7ac992bfd6c1cdabda0e53f1ba8dc7e450095280
dawnIceZhu <Hello16369@163.com>
2022-07-07 20:46:19 +0800

    feat: 带余除法

--- func.go
+++ func.go
@@ -3,21 +3,34 @@ package main
 import "fmt"
 
 // 函数基础使用
-func eval(a, b int, op string) int {
+func eval(a, b int, op string) (int, error) {
 	switch op {
 	case "+":
-		return a + b
+		return a + b, nil
 	case "-":
-		return a - b
+		return a - b, nil
 	case "*":
-		return a * b
+		return a * b, nil
 	case "/":
-		return a / b
+		q, _ := div(a, b)
+		return q, nil
 	default:
-		panic("unsupported operation: " + op)
+		return 0, fmt.Errorf(
+			"unsupported operation: %s", op)
 	}
 }
 
+// 带余除法
+func div(a, b int) (q, r int) {
+	return a / b, a % b
+}
+
 func main() {
-	fmt.Println(eval(2, 3, "="))
+	if result, err := eval(13, 3, "X"); err != nil {
+		fmt.Println("Error: ", err)
+	} else {
+		fmt.Println(result)
+	}
+	q, r := div(13, 3)
+	fmt.Println(q, r)
 }
