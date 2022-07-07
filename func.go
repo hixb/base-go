d0268f5791f20789858f83f00b2ffe970c518366
dawnIceZhu <Hello16369@163.com>
2022-07-07 20:33:48 +0800

    feat: 函数基础使用

---
+++ func.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// 函数基础使用
+func eval(a, b int, op string) int {
+	switch op {
+	case "+":
+		return a + b
+	case "-":
+		return a - b
+	case "*":
+		return a * b
+	case "/":
+		return a / b
+	default:
+		panic("unsupported operation: " + op)
+	}
+}
+
+func main() {
+	fmt.Println(eval(2, 3, "="))
+}
