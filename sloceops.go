6907a6ba226b6daa3d22bf0f8c3f232a66d095fc
zhuxb <Hello163692163.com>
2022-07-08 22:18:35 +0800

    feat: 创建切片

---
+++ sloceops.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func printSlice(s []int) {
+	fmt.Printf("%v, len=%d, cap=%d\n", s, len(s), cap(s))
+}
+
+func main() {
+	fmt.Println("Creating slice ===============================")
+	var s []int // zero value for slice is nil
+
+	for i := 0; i < 100; i++ {
+		printSlice(s)
+		s = append(s, 2*i+1)
+	}
+	fmt.Println(s)
+
+	s1 := []int{2, 4, 6, 8}
+	printSlice(s1)
+
+	s2 := make([]int, 16)
+	s3 := make([]int, 10, 32)
+	printSlice(s2)
+	printSlice(s3)
+}
